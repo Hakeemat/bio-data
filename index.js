325d79881f4fb4da41ade15365a68334a5df6235
Hakeemat <hakeemttijani@gmail.com>
2022-06-23 12:48:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let operator = prompt("Enter operator:+,-,*,/")
+let num1 = parseInt (prompt('Num1: '))
+let num2 = parseInt (prompt('Num2: '))
+
+if (operator == '+'){
+    alert(num1 + num2);
+ }
+ else if (operator == '-')
+        {alert(num1 - num2);
+ }
+ else if (operator == '*')
+        {alert(num1 * num2);
+ }
+ else if (operator == '/')
+        {alert(num1 / num2);
+ }
+
